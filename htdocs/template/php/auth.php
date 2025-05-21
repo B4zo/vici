59ad121c0f0e648e8c107fe36e26ef8357e41caf
B4zo <aljaz-skafar@outlook.com>
2025-05-21 09:30:48 +0200

    Fixed template/php/auth.php

--- htdocs/template/php/auth.php
+++ htdocs/template/php/auth.php
@@ -6,12 +6,13 @@ if (!isset($_SESSION['userUUID']) && isset($_COOKIE['rememberme'])) {
 
   require_once 'db.php';
 
-  $sqlCookie = "SELECT userUUID, admin FROM sessions WHERE token = '$tokenHashed'";
+  $sqlCookie = "SELECT sessions.userUUID, users.username, users.admin FROM sessions JOIN users ON sessions.userUUID = users.userUUID WHERE sessions.token = '$tokenHashed'";
   $resultCookie = mysqli_query($link, $sqlCookie);
 
   if ($resultCookie && mysqli_num_rows($resultCookie) === 1) {
       $row = mysqli_fetch_assoc($resultCookie);
       $_SESSION['userUUID'] = $row['userUUID'];
+      $_SESSION['username'] = $row['username'];
       if($row['admin'] == 1) {
         $_SESSION['admin'] = 1;
       }
